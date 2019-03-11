50a1fb3869dea478f0e0ff25605ff9854d7a076e
Moaath Alrajab <moaath@alrajab.net>
2019-03-11 00:32:37 -0400

    Adding main and start methods

--- eclipse-workspace/Week6Lec2/DemoProj/src/fd.java
+++ eclipse-workspace/Week6Lec2/DemoProj/src/fd.java
@@ -1,4 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class fd {
+public class fd extends Application{
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
